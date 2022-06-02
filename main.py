0e4678fdb4c6c97953ac05f5cb0364f67b720fd9
Mohit Chauhan <101303720+mohitchauhan3@users.noreply.github.com>
2022-06-02 21:19:09 +0000

    asking the first question

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#1.0
 print("Kia Ora Ko Wai Tou Ingoa? / meaning Hello What is your name?")
 n = input()
 print("Kia Ora / hello " + n )
 
+# Asking The user name in te reo 
+
 score = 0
+
+answer1 = print("What is the term for Instagram? \na: Paeāhua \nb: Tīhau \nc: Pukamata \nd: Pāhorangi")
+
