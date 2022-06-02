069ec4dc56749011f36000129565129a4475dd5e
Mohit Chauhan <101303720+mohitchauhan3@users.noreply.github.com>
2022-06-02 21:53:14 +0000

    adding question 3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Kia Ora / hello " + n )
 
 score = 0
 
-#question 1
+#Question 1
 answer1 = input("What is the term for Instagram? \na. Paeāhua \nb. Tīhau \nc. Pukamata \nd. Pāhorangi \nAnswer: ")
 
 if answer1 == "a" or answer1 == "A":
@@ -20,6 +20,18 @@ else:
   print("score: ", score)
   print("\n")
 
+#Question 2
+answer2 = input("What is the term for Twitter? \na. Tīhau \nb Paeāhua \nc. Ahokore \nd.kiriāhua: ")  
+if answer2 == "a" or answer2 == "A":
+  score += 1
+  print("CORRECT!")
+  print("score: ", score)
+  print("\n")
+else:
+  print("INCORRECT! The answer is A: Tīhau")
+  print("score: ", score)
+  print("\n")
 
+#Question 3  
 
 
