aa31eec894bfc28a56135ff54be8b3d9079faf5d
Mohit Chauhan <101303720+mohitchauhan3@users.noreply.github.com>
2022-06-02 21:42:20 +0000

    chanage print to input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#1.1
+#1.2
 print("Kia Ora Ko Wai Tou Ingoa? / meaning Hello What is your name?")
 n = input()
 print("Kia Ora / hello " + n )
@@ -7,12 +7,18 @@ print("Kia Ora / hello " + n )
 
 score = 0
 
-answer1 = print("What is the term for Instagram? \na: Paeāhua \nb: Tīhau \nc: Pukamata \nd: Pāhorangi")
-if answer1 == "a" or answer1 == "A" or answer1 == "Paeahua":
+#question 1
+answer1 = input("What is the term for Instagram? \na. Paeāhua \nb. Tīhau \nc. Pukamata \nd. Pāhorangi \nAnswer: ")
+
+if answer1 == "a" or answer1 == "A":
    score += 1
-  print("CORRECT!")
-print("score: ", score)
-print("\n")
+   print("CORRECT!")
+   print("score: ", score)
+   print("\n")
 else:
+  print("INCORRECT! The answer is A: Paeāhua")
+  print("score: ", score)
+  print("\n")
+
 
 
